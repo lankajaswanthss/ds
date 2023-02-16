98d9359f2e890afdd82f096c6b23577f8780737f
lankajaswanthss <112810058+lankajaswanthss@users.noreply.github.com>
2023-02-16 21:59:52 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,11 +2,15 @@
 self.addEventListener('message', function(event) {
     // If the message is to start tracking location
     if (event.data.type === 'start') {
-      // Start tracking location
-      var watchId = navigator.geolocation.watchPosition(function(position) {
-        // Send the location back to the main thread
-        self.postMessage({lat: position.coords.latitude, lng: position.coords.longitude});
-      });
+      if ('geolocation' in navigator) {
+        // Start tracking location
+        var watchId = navigator.geolocation.watchPosition(function(position) {
+          // Send the location back to the main thread
+          self.postMessage({lat: position.coords.latitude, lng: position.coords.longitude});
+        });
+      } else {
+        // Geolocation API is not supported
+        self.postMessage({error: 'Geolocation API is not supported'});
+      }
     }
   });
-  
