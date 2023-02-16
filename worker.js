a978f454f475e0379eae1ae77107ed56d416873e
lankajaswanthss <112810058+lankajaswanthss@users.noreply.github.com>
2023-02-16 22:24:25 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,16 +1,21 @@
 // Listen for messages from the main thread
 self.addEventListener('message', function(event) {
-    // If the message is to start tracking location
-    if (event.data.type === 'start') {
-      if ('geolocation' in navigator) {
-        // Start tracking location
-        var watchId = navigator.geolocation.watchPosition(function(position) {
-          // Send the location back to the main thread
-          self.postMessage({lat: position.coords.latitude, lng: position.coords.longitude});
-        });
-      } else {
-        // Geolocation API is not supported
-        self.postMessage({error: 'Geolocation API is not supported'});
-      }
+  // If the message is to start tracking location
+  if (event.data.type === 'start') {
+    if ('geolocation' in navigator) {
+      // Start tracking location
+      var watchId = navigator.geolocation.watchPosition(function(position) {
+        // Send the location back to the main thread
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        self.postMessage({type: 'location', latitude: latitude, longitude: longitude});
+      }, function(error) {
+        // Send the error message back to the main thread
+        self.postMessage({type: 'error', message: error.message});
+      });
+    } else {
+      // Geolocation API is not supported
+      self.postMessage({type: 'error', message: 'Geolocation API is not supported'});
     }
-  });
+  }
+});
